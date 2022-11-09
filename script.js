e1aad339c6b72540af053512f053fc78dad5a7d4
gaborio1 <welcome2thepleasuredome@googlemail.com>
2022-11-09 23:15:28 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("connected");
+
+const commonWords = ["the", "of", "and", "a", "to", "in", "is", "you", "that", "it", "he", "was", "for", "on", "are", "as", "with", "his", "they", "I", "at", "be", "this", "have", "from", "or", "one", "had", "by", "word", "but", "not", "what", "all", "were", "we", "when", "your", "can", "said", "there", "use", "an", "each", "which", "she", "do", "how", "their", "if", "will", "up", "other", "about", "out", "many", "then", "them", "these", "so", "some", "her", "would", "make", "like", "him", "into", "time", "has", "look", "two", "more", "write", "go", "see", "number", "no", "way", "could", "people", "my", "than", "first", "water", "been", "call", "who", "oil", "its", "now", "find", "long", "down", "day", "did", "get", "come", "made", "may", "part"];
+
+const sequenceLength = 30;
+
+// GET RANDOM WORD FROM ARRAY
+const getRandom = (arr) => {
+    const randIdx = Math.floor(Math.random() * commonWords.length);
+    return commonWords[randIdx];
+}
+
+console.log(getRandom());
+
+const textContainer = document.querySelector(".text-container");
+textContainer.textContent = getRandom();
+
+// CREATE TEXT STRING FROM RANDOM WORDS UP TO sequenceLength IN LENGTH
+let stringWords = "";
+let wordsArr = [];
+
+// GET CURRENT LENGTH OF WORDS PLUS SPACES IN BETWEEN
+
+
+while (stringWords.length < sequenceLength) {
+    stringWords += getRandom();
+    wordsArr.push(getRandom());
+}
+console.log(wordsArr);
+console.log(stringWords);
+
